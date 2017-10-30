08f9bff05d1fdc2b07ba2671747d99049f377701
stephangrundner <rukyg8seph>
2017-10-30 08:22:15 +0100

    Enhancements & Bug fixes

--- src/main/java/biz/jovido/seed/content/AliasRequestMapping.java
+++ src/main/java/biz/jovido/seed/content/AliasRequestMapping.java
@@ -1,5 +1,7 @@
 package biz.jovido.seed.content;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.StringUtils;
 import org.springframework.web.servlet.HandlerExecutionChain;
@@ -12,6 +14,8 @@ import javax.servlet.http.HttpServletRequest;
  */
 public class AliasRequestMapping extends AbstractHandlerMapping {
 
+    private static final Logger LOG = LoggerFactory.getLogger(AliasRequestMapping.class);
+
     @Autowired
     private AliasForwardController aliasForwardController;
 
@@ -23,9 +27,24 @@ public class AliasRequestMapping extends AbstractHandlerMapping {
 
     @Override
     protected Object getHandlerInternal(HttpServletRequest request) throws Exception {
+        String serverName = request.getServerName();
+        String localName = request.getLocalName();
+        String localAddr = request.getLocalAddr();
+        String remoteAddr = request.getRemoteAddr();
+        String requestURI = request.getRequestURI();
+        String requestURL = request.getRequestURL().toString();
+
+        LOG.debug("serverName: {}", serverName);
+        LOG.debug("localName: {}", localName);
+        LOG.debug("localAddr: {}", localAddr);
+        LOG.debug("remoteAddr: {}", remoteAddr);
+        LOG.debug("requestURI: {}", requestURI);
+        LOG.debug("requestURL: {}", requestURL);
+
         Host host = hostService.getHost(request.getServerName());
         if (host != null) {
             String path = request.getServletPath();
+            LOG.debug("Request path: {}", path);
             if ("/".equals(path)) {
                 path = host.getPath();
             } else if (StringUtils.hasLength(path) && path.startsWith("/")) {
