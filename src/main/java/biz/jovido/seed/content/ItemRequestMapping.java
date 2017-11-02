e16cdd2ed321f512d58e7e72f108df00c8dfcb62
stephangrundner <rukyg8seph>
2017-11-02 21:48:06 +0100

    Enhancements & Bug fixes

--- src/main/java/biz/jovido/seed/content/ItemRequestMapping.java
+++ src/main/java/biz/jovido/seed/content/ItemRequestMapping.java
@@ -29,27 +29,41 @@ public class ItemRequestMapping extends AbstractHandlerMapping {
         String hostName = request.getHeader("X-Forwarded-Host");
         if (StringUtils.isEmpty(hostName)) {
             hostName = request.getServerName();
+        } else {
+            LOG.info("X-Forwarded-Host: {}", hostName);
         }
-        hostName = hostService.getHostName(hostName);
+
+        if (!"localhost".equals(hostName)) {
+            hostName = hostService.getHostName(hostName);
+            LOG.info("Host: {}", hostName);
+        } else {
+            LOG.info("Host: localhost");
+        }
+
         Host host = hostService.getHost(hostName);
         if (host != null) {
             String path = request.getServletPath();
             if ("/".equals(path)) {
                 path = host.getPath();
+                LOG.info("Request path: /");
             } else if (StringUtils.hasLength(path) && path.startsWith("/")) {
                 path = path.substring(1);
+                LOG.info("Request path: {}", path);
             }
 
             List<Item> items = itemService.findAllPublishedItemByPath(path);
             if (!items.isEmpty()) {
                 if (items.size() > 1) {
-                    LOG.warn("More than one published item found for path [{}]", path);
+                    LOG.warn("More than one published item found for path {}", path);
                 }
 //                TODO Find best matching item:
                 Item item = items.get(0);
+                LOG.info("Item {} found for path {}", item.getId(), path);
                 ItemForwardController controller =
                         new ItemForwardController(item);
                 return new HandlerExecutionChain(controller);
+            } else {
+                LOG.warn("No item found for path {}", path);
             }
         }
 
